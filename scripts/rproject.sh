f4b7bf97e988504bfa038595f45adeb151fa9066
ijapesigan <ijapesigan@gmail.com>
2025-11-02 23:31:37 +0000

    Automated build.

--- scripts/rproject.sh
+++ scripts/rproject.sh
@@ -3,24 +3,25 @@
 set -e
 
 install2.r --error --skipinstalled -n -1 \
-    cffr       \
-    covr       \
-    devtools   \
-    distro     \
-    knitr      \
-    lintr      \
-    pkgdown    \
-    quarto     \
-    Rcpp       \
-    remotes    \
-    RefManageR \
-    rmarkdown  \
-    roxygen2   \
-    rprojroot  \
-    stats      \
-    styler     \
-    tinytex    \
-    utils      \
+    BiocManager \
+    cffr        \
+    covr        \
+    devtools    \
+    distro      \
+    knitr       \
+    lintr       \
+    pkgdown     \
+    quarto      \
+    Rcpp        \
+    remotes     \
+    RefManageR  \
+    rmarkdown   \
+    roxygen2    \
+    rprojroot   \
+    stats       \
+    styler      \
+    tinytex     \
+    utils       \
     testthat
 
 R -e "remotes::install_github(      \
