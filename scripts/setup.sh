27b5c252d48fd7e71013bca6ef23af3615375bb8
ijapesigan <ijapesigan@gmail.com>
2025-12-22 06:39:16 +0000

    Automated build.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-# If buildkit secret is mounted, expose it to the environment for R to use.
 if [ -f /run/secrets/GITHUB_PAT ]; then
-  export GITHUB_PAT="$(cat /run/secrets/GITHUB_PAT)"
+    GITHUB_PAT=$(cat /run/secrets/GITHUB_PAT)
+    export GITHUB_PAT
 fi
 
-# Optional safe debug (do not print the token):
 if [ -n "$GITHUB_PAT" ]; then
-  echo "GITHUB_PAT is set (length=$(echo -n $GITHUB_PAT | wc -c))"
+    echo "GITHUB_PAT is set (length=$(echo -n "$GITHUB_PAT" | wc -c))"
 else
-  echo "GITHUB_PAT NOT set"
+    echo "GITHUB_PAT NOT set"
 fi
 
 bash ubuntu_sys_pkg.sh
