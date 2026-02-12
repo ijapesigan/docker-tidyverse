5106c64728f6ec55c5e1329cb8f1c4d9f68e2a64
jeksterslab <learn.jeksterslab@gmail.com>
2026-02-12 20:23:46 +0000

    Automated build.

--- scripts/rproject.sh
+++ scripts/rproject.sh
@@ -25,16 +25,6 @@ install2.r --error --skipinstalled -n -1 \
     utils       \
     testthat
 
-R -e "install.packages(                                   \
-       'pak',                                             \
-       repos = sprintf(                                   \
-         'https://r-lib.github.io/p/pak/stable/%s/%s/%s', \
-         .Platform$pkgType,                               \
-         R.Version()$os,                                  \
-         R.Version()$arch                                 \
-       )                                                  \
-     )"
-
 R -e "remotes::install_github(      \
     c(                              \
         'ijapesigan/rProject'       \
